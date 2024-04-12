3834bbb522f1b7bffd4b6bb3986ba7580d239b05
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-12 15:10:53 +0000

    u

--- js/script.js
+++ js/script.js
@@ -4,14 +4,12 @@
 
 
 "use strict"
-// function to calculate degrees fahrenheit to celsius
-function calculateCelsius() {
-  // declaring variable for degrees fahrenheit
-  let f = document.getElementById("f").value;
 
-  // calculates degrees celcius by multiplying degrees fahrenheit subtracted by 32, by 5/9
-  let celsius = (f-32) * (5/9);
-  document.getElementById("user-info").innerHTML = f + "° fahrenheit is equal to " + celsius.toFixed(0) + "°C.";
+kilometer() {
 
-  return celsius;
+Let unitOfMeasurement = getElementById("units").value;
+  
+let unitOfMeasuremen = "kilometers";
+
+
 }
