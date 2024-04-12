23f67cf22d67bd697f1899f0bd627d9fe59a5112
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-12 00:07:52 +0000

    u

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Niko
+// Created on: april 2024
 // This file contains the JS functions for index.html
+
+
+"use strict"
+// function to calculate degrees fahrenheit to celsius
+function calculateCelsius() {
+  // declaring variable for degrees fahrenheit
+  let f = document.getElementById("f").value;
+
+  // calculates degrees celcius by multiplying degrees fahrenheit subtracted by 32, by 5/9
+  let celsius = (f-32) * (5/9);
+  document.getElementById("user-info").innerHTML = f + "° fahrenheit is equal to " + celsius.toFixed(0) + "°C.";
+
+  return celsius;
+}
