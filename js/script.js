e6556facea2993e8b36611ce6b972436b992751f
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-13 00:27:53 +0000

    u

--- js/script.js
+++ js/script.js
@@ -7,14 +7,31 @@
 
 let inputBox = document.getElementById("kilometers")
 function kilometer() {
-  inputBox.setAttribute("placeholder", "kilometers")
+  inputBox.setAttribute("placeholder", "Kilometers")
+  document.getElementById("units").innerHTML = "Kilometers: "
 }
 function meter() {
-  inputBox.setAttribute("placeholder", "meters")
+  inputBox.setAttribute("placeholder", "Meters")
+  document.getElementById("units").innerHTML = "Meters: "
 }
 
+function convertk() {
+
+  let kilometers = document.getElementById("").value;
+  
+  // Perform calculation using the formula for kilometers to meters
+  let meters = kilometers * 1000;
+  resultContainer.textContent = "Result: " + kilometers + "km is equal to " + meters + "m.";
+  }
 
 
+function convertm() {
+
+  let meters = document.getElementById("").value;
+  // Perform calculation using the formula for meters to feet
+  let feet = meters * 3.28084
+  resultContainer.textContent = "Result: " + meters + "m is equal to " + feet + "ft.";
+  }
 
 
 
