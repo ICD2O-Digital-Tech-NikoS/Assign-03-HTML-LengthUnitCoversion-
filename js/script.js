3995ccaf00ed3c2141dc65e3437653d0e848fe6b
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-16 01:14:44 +0000

    u

--- js/script.js
+++ js/script.js
@@ -11,7 +11,12 @@ function convertk() {
   
   // Perform calculation using the formula for kilometers to meters
   let meters = kilometers * 1000;
-  document.getElementById("num-of-meters").innerHTML = "Result: " + kilometers + "km is equal to " + meters + "m.";
+  document.getElementById("num-of-meters").innerHTML = "Result: " + kilometers + "km is equal to " + meters.toFixed(2) + "m.";
+
+  if (kilometers == 0) {
+    document.getElementById("num-of-meters").innerHTML = "Error: Please enter a number greater than 0.";
+  }
+
   }
 
 
@@ -20,9 +25,13 @@ function convertm() {
   let meters = document.getElementById("meters").value;
   // Perform calculation using the formula for meters to feet
   let feet = meters * 3.28084
-document.getElementById("num-of-feet").innerHTML = "Result: " + meters + "m is equal to " + feet + "ft.";
+document.getElementById("num-of-feet").innerHTML = "Result: " + meters + "m is equal to " + feet.toFixed(2) + "ft.";
+
+  if (meters == 0) {
+    document.getElementById("num-of-feet").innerHTML = "Error: Please enter a number greater than 0.";
   }
 
+  }
 
 
 
